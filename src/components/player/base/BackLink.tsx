2d202f72e91c7c2b4dea1d1c98aceff7fb786417
Antoncabon <antoncabon@users.noreply.github.com>
2025-03-17 22:16:18 +0700

    Update BackLink.tsx

--- src/components/player/base/BackLink.tsx
+++ src/components/player/base/BackLink.tsx
@@ -8,14 +8,7 @@ export function BackLink(props: { url: string }) {
 
   return (
     <div className="flex items-center">
-      <Link
-        to={props.url}
-        className="py-1 -my-1 px-2 -mx-2 tabbable rounded-lg flex items-center cursor-pointer text-type-secondary hover:text-white transition-colors duration-200 font-medium"
-      >
-        <Icon className="mr-2" icon={Icons.ARROW_LEFT} />
-        <span className="md:hidden">{t("player.back.short")}</span>
-        <span className="hidden md:block">{t("player.back.default")}</span>
-      </Link>
+      
     </div>
   );
 }
